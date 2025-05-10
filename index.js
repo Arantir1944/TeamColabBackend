ca65421e342ebbf608b06bec4175f9ef7c697c33
Arantir1944 <bogdan1necula@yahoo.com>
2025-05-10 10:29:52 +0300

    Updated DB variables

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // index.js
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
 const express = require("express");
 const http = require("http");           // Required for Socket.io
 const cors = require("cors");
