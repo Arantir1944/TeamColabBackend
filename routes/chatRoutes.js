26614355bacd5dc6db1ea36331208b82fd867fb2
Arantir1944 <bogdan1necula@yahoo.com>
2025-05-22 22:11:55 +0300

    Update chatRoutes.js

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const authenticate = require('../middleware/authMiddleware');
 const chatController = require('../controllers/chatController');
+const { searchUsers } = require('../controllers/userController');
+
 
 // all of these require a valid JWT
 router.use(authenticate);
@@ -11,6 +13,7 @@ router.post('/conversations/direct', chatController.createDirectConversation);
 router.post('/conversations/group', chatController.createGroupConversation);
 router.get('/conversations', chatController.getUserConversations);
 router.get('/conversations/:id', chatController.getConversationWithMessages);
+router.get("/", searchUsers); // handles ?search=query
 
 // sending a message
 router.post('/messages', chatController.sendMessage);
